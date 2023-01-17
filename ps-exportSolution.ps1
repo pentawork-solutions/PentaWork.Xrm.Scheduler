9784e9b08b900f500a05f574db0f97759207f478
Gerrit Gazic <gerrit.gazic@pentawork.de>
2023-01-17 09:52:15 +0100

    Update Solution

--- ps-exportSolution.ps1
+++ ps-exportSolution.ps1
@@ -5,4 +5,5 @@ $solutionFolder = $PSScriptRoot + "\solution\"
 
 $conn = Get-CrmConnection -InteractiveMode
 $solutionFile = Export-XrmSolution -Connection $conn -UniqueName $solutionName -ExportPath $env:temp -PublishAll
+Remove-Item -Recurse -Force $solutionFolder
 Expand-Archive $solutionFile -DestinationPath $solutionFolder
